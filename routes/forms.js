4297cf0526fe852bef040c5324ab5f248cc69672
Jacob Li Peng Cheng <lipech@hotmail.com>
2019-03-11 17:28:57 +0800

    use partials for index, about, forms and select pages

--- routes/forms.js
+++ routes/forms.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('forms', { title: 'Forms and Interaction' });
+	res.render('forms', { title: 'Admin Form' });
 });
 
 module.exports = router;
