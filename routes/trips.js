fc4706f404a3892f1cfca5bdfbaa43fb31b54c2c
zacci <isaacgtan57@gmail.com>
2019-04-06 21:56:16 +0800

    Added Trips Page
    Also changed data type for test data for Monetary Values from Integer to Real, which supports decimals.

---
+++ routes/trips.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+const { Pool } = require('pg') // postgres database package
+/* --- V7: Using Dot Env ---
+// change the password specifically for your own database
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '********',
+  port: 5432,
+})
+*/
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+/* SQL Query */
+var sql_query = 'SELECT * FROM trips';
+router.get('/', isLoggedIn, function(req, res, next) {
+	pool.query(sql_query, (err, data) => {
+		if (err) {
+			next(err);
+		}
+		else{
+			res.render('trips', {
+				title: 'Trips List',
+				user : req.user,
+				data: data.rows
+			});
+		}
+	});
+});
+
+module.exports = router;
